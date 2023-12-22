7d835ae8753ca513f6f3da5ef579a250a75828cb
边上海 <bshsf@qq.com>
2023-12-22 23:57:28 +0800

    [update] 实现点击blocker 可以shack 或者关闭面板

--- Packages/com.zframework.uiblocker/Samples/Example/NotificationPanel.cs
+++ Packages/com.zframework.uiblocker/Samples/Example/NotificationPanel.cs
@@ -12,7 +12,7 @@ namespace zFramework.Example
         public Text content;
         public Button confirmButton;
         public Button cancelButton;
-
+        public bool closeByBlock = false;
         public async UniTask<int> ShowAsync(string title, string content)
         {
             this.title.text = title;
@@ -20,18 +20,24 @@ namespace zFramework.Example
             // reset panel 
             transform.localScale = Vector3.one * 0.1f;
             gameObject.SetActive(true);
-            await this.BlockAsync(Color.black, 0.8f, 0.5f, 0.3f); // must blocker first
+            await this.BlockAsync(Color.black, 0.8f, 0.3f, 0.3f); // must blocker first
             await transform.DoScaleAsync(Vector3.one, 0.5f, Ease.OutBack);
 
             var index = await UniTask.WhenAny(confirmButton.OnClickAsync(), cancelButton.OnClickAsync());
-            await transform.DoScaleAsync(Vector3.one * 0.1f, 0.5f, Ease.InBack);
+            _ = transform.DoScaleAsync(Vector3.one * 0.01f, 0.3f, Ease.Linear);
+            await this.UnblockAsync(0.5f);
             gameObject.SetActive(false);
-            _ = this.UnblockAsync(0.5f);
             return index; // result should never be wait
         }
 
         public async UniTask<bool> HandleBlockClickedAsync()
         {
+            if (closeByBlock)
+            {
+                await transform.DoScaleAsync(Vector3.one * 0.1f, 0.3f, Ease.InBack);
+                gameObject.SetActive(false);
+                return true;
+            }
             await transform.DoShackPositionAsync(0.3f, Vector3.one * 20);
             return false;
         }
