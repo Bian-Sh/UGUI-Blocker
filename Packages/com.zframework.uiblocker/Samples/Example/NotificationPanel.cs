0c859e916feffb58677d870abbb1b32a2a9d1107
边上海 <bshsf@qq.com>
2023-12-24 05:30:29 +0800

    [update] 完善 demo 的细节

--- Packages/com.zframework.uiblocker/Samples/Example/NotificationPanel.cs
+++ Packages/com.zframework.uiblocker/Samples/Example/NotificationPanel.cs
@@ -1,4 +1,6 @@
 using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 using zFramework.Anim;
@@ -12,34 +14,68 @@ namespace zFramework.Example
         public Text content;
         public Button confirmButton;
         public Button cancelButton;
+        public Toggle toggle;
+        private CancellationTokenSource cts;
+
+        private void Start() => toggle.onValueChanged.AddListener((value) => closeByBlock = value);
+
         public bool closeByBlock = false;
+        public bool useBlocker = true;
         public async UniTask<int> ShowAsync(string title, string content)
         {
+            cts = new CancellationTokenSource();
             this.title.text = title;
             this.content.text = content;
+
             // reset panel 
             transform.localScale = Vector3.one * 0.1f;
             gameObject.SetActive(true);
-            await this.BlockAsync(Color.black, 0.8f, 0.3f, 0.3f); // must blocker first
-            await transform.DoScaleAsync(Vector3.one, 0.5f, Ease.OutBack);
+            var index = -1;
+            try
+            {
+                // must blocker first, other wise you may click the other button before the panel fadein
+                // delay 0.1f means wait for panel show about 0.1f then blocker start fadein
+                // You set the block fade-in duration to 0.3f and delay to 0.1f, so the blocker will appear along with the panel suddenly.
+                if (useBlocker) await this.BlockAsync(Color.black, 0.8f, 0.3f, 0.1f);
 
-            var index = await UniTask.WhenAny(confirmButton.OnClickAsync(), cancelButton.OnClickAsync());
-            _ = transform.DoScaleAsync(Vector3.one * 0.01f, 0.3f, Ease.InBack);
-            await this.UnblockAsync(0.5f);
-            gameObject.SetActive(false);
-            return index; // result should never be wait
+                await transform.DoScaleAsync(Vector3.one, 0.5f, Ease.OutBack);
+                index = await UniTask.WhenAny(confirmButton.OnClickAsync(cts.Token), cancelButton.OnClickAsync(cts.Token));
+
+                // if you want blocker fadeout along with panel , you should use "_= " to make them run in parallel
+                _ = transform.DoScaleAsync(Vector3.one * 0.01f, 0.5f, Ease.InBack);
+                //If the panel fadeout duration is less than that of the blocker, the blocker will fade out first and then the panel will suddenly become inactive.
+                // so that blocker fadeout duration should same to panel fadeout duration
+                await this.UnblockAsync(0.5f);
+                gameObject.SetActive(false);
+            }
+            catch (Exception e) when (e is OperationCanceledException)
+            {
+                // if  exception is OperationCanceledException,do nothing
+            }
+            return index; // result should never be wait 
         }
 
-        public async UniTask<bool> HandleBlockClickedAsync()
+        public async UniTask<Blocker.Context> HandleBlockClickedAsync()
         {
             if (closeByBlock)
             {
-                await transform.DoScaleAsync(Vector3.one * 0.1f, 0.3f, Ease.InBack);
-                gameObject.SetActive(false);
-                return true;
+                // if you want blocker fadeout along with panel
+                // you may need a local async method to do that , only in this way you can hide gameobject after it scale to 0.1f
+                // which is as same as DoTween.DOFade().OnComplete(()=>gameObject.SetActive(false));  
+                async void HideAsync()
+                {
+                    await transform.DoScaleAsync(Vector3.one * 0.1f, 0.3f, Ease.InBack);
+                    gameObject.SetActive(false);
+                }
+                // Tell ShowAsync()  logic that you want to close panel ,  showasync will early out ,
+                // if not , UniTask.WhenAny will execute multiple times next time you call ShowAsync 
+                cts?.Cancel();
+
+                HideAsync();
+                return new Blocker.Context { close = true, duration = 0.5f };
             }
             await transform.DoShackPositionAsync(0.3f, Vector3.one * 20);
-            return false;
+            return default; // default is ok , means do not close blocker
         }
     }
 }
--- Packages/com.zframework.uiblocker/Samples/Example/PanelController.cs
+++ Packages/com.zframework.uiblocker/Samples/Example/PanelController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using zFramework.Example;
@@ -5,23 +6,37 @@ using zFramework.Example;
 public class PanelController : MonoBehaviour
 {
     public Button button;
+    public Button button2;
     public NotificationPanel panel;
+    public NotificationPanel panel2;
 
     private void Start()
     {
         button.onClick.AddListener(OnClick);
+        button2.onClick.AddListener(OnClick2);
     }
 
-    public async void OnClick()
+    // Open a panel which is not blocked by blocker
+    private async void OnClick2()
     {
-        Debug.Log("Clicked");
-        if (!panel.gameObject.activeSelf)
+        if (!panel2.gameObject.activeSelf)
         {
-            var title = "Title";
-            var content = "Content 啊！";
-            var idx = await panel.ShowAsync(title, content);
-            Debug.Log("idx: " + idx);
+            var title = "Panel without a Blocker";
+            var content = "This panel will be overlaid by other UI as it does not use a blocker！";
+            var idx = await panel2.ShowAsync(title, content);
+            Debug.Log("user selected : " + (idx == 0 ? "确定" : (idx == -1 ? "用户取消操作" : "取消")));
         }
     }
 
+    // Open a panel which is blocked by blocker
+    public async void OnClick()
+    {
+        if (!panel.gameObject.activeSelf)
+        {
+            var title = "Panel with a Blocker";
+            var content = "This panel will be rendered on the top layer！";
+            var idx = await panel.ShowAsync(title, content);
+            Debug.Log("user selected : " + (idx == 0 ? "确定" : (idx == -1 ? "用户取消操作" : "取消")));
+        }
+    }
 }
